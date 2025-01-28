6a641977406700b3b98db35da3d8dcd4f53109d9
Czesky93 <rafal.kluczewski.info@gmail.com>
2025-01-28 22:03:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def analyze(username):
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Użycie dynamicznego portu dla Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
