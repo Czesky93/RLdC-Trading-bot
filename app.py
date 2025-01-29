2f7f4dde5eda9a393a6f9261c03c23f467b3812e
Czesky93 <rafal.kluczewski.info@gmail.com>
2025-01-29 12:05:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,6 @@ def dynamic_dashboard_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
